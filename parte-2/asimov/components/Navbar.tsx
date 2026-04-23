d7ea1ee2901e713ae63d79546a0a1e124a064202
Amanda Oliveira <amandaol7b@gmail.com>
2026-04-23 10:45:12 -0300

    feat: add navbar

---
+++ parte-2/asimov/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import { Button } from "./Button";
+
+export function Navbar() {
+  return (
+    <nav className="py-8 flex items-center justify-between bg-color-foreground">
+        <h1 className="logo text-2xl">ASIMOV</h1>
+        <ul className="flex items-center gap-8 text-secondary-text">
+            <li className="hover:text-primary cursor-pointer transition-colors">CURRÍCULO</li>
+            <li className="hover:text-primary cursor-pointer transition-colors">INSTRUTORES</li>
+            <li className="hover:text-primary cursor-pointer transition-colors">CURSOS</li>
+            <li className="hover:text-primary cursor-pointer transition-colors">BLOG</li>
+        </ul>
+        <div>
+            <Button variant="text" label="Entrar" />
+            <Button variant="primary" label="Cadastrar" />
+        </div>
+    </nav>
+  );
+}
